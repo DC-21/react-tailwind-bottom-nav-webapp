6b9de4e337c4bb921f9765c09342847b11f08220
DC-21 <cholahkuboko@gmail.com>
2023-07-18 11:52:12 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className='flex w-full h-screen px-3 py-2'>Hero</div>
+    <div className='bg-[#60c0d2] flex w-full h-screen px-3 py-2 justify-center items-center'>
+        <div className='w-full h-full flex justify-center'>
+            <div className='justify-center items-center flex w-1/2'>
+                <p className='text-left font-bold text-4xl'>Design and Build Stunning World Class Websites With Us</p>
+            </div>
+            <div className='w-1/2'></div>
+        </div>
+    </div>
   )
 }
 
