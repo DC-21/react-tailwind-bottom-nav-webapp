ddee971eb16351e90f4f9c191e6eb22930954e7f
DC-21 <cholahkuboko@gmail.com>
2023-07-18 11:30:02 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import Bottom from './components/Bottom'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Bottom from "./components/Bottom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <>
-    <Bottom/>
+      <Bottom />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
