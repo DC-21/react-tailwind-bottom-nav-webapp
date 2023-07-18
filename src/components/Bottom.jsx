30e185b404ee55cbc94caa820d1c377c6a2858d0
DC-21 <cholahkuboko@gmail.com>
2023-07-18 09:39:45 +0200

    first commit

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Bottom = () => {
+  const [active, setActive] = useState(0);
   return (
-    <div className="w-full py-4 px-6 sticky bottom-0 justify-center">
-      <div className="w-[450px] flex bottom-0 sticky gap-12 bg-[#1f2a40] justify-center py-3 text-white rounded-3xl">
-        <a className="flex flex-col items-center gap-2 cursor-pointer">
+    <div className="fixed w-full py-4 px-6 bottom-0 justify-center">
+      <div className="w-[450px] flex bottom-0 sticky gap-12 bg-[#1f2a40] justify-center items-center py-3 text-white rounded-3xl">
+        <a className="flex flex-col items-center gap-2 cursor-pointer" onClick={()=> setActive()}>
           <svg viewBox="0 0 24 24" fill="currentColor" height="2em" width="2em">
             <path d="M5 22h14a2 2 0 002-2v-9a1 1 0 00-.29-.71l-8-8a1 1 0 00-1.41 0l-8 8A1 1 0 003 11v9a2 2 0 002 2zm5-2v-5h4v5zm-5-8.59l7-7 7 7V20h-3v-5a2 2 0 00-2-2h-4a2 2 0 00-2 2v5H5z" />
           </svg>
