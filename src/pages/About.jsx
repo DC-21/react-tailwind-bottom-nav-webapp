4491e747973cd4bd0161f804e44e3e4a01ae269f
DC-21 <cholahkuboko@gmail.com>
2023-07-19 09:09:50 +0200

    first commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,15 +4,16 @@ import own from '../images/own.png';
 const About = () => {
   return (
     <div className='w-full flex justify-center items-center md:h-screen h-auto bg-[#f6a98c]'>
-        <div className='w-full md:flex justify-center items-center'>
+        <div className='w-full md:flex justify-center items-center px-6 py-4'>
             <div className='w-1/2 flex items-center justify-center'>
-                <div>
-                    <p>Hello</p>
+                <div className='flex flex-col w-full'>
+                    <h1 className='font-bold text-4xl'>About</h1>
+                    <span className='pt-3 flex gap-3'>I am<p>Chola Kuboko</p></span>
                 </div>
             </div>
             <div className='w-1/2 flex items-center justify-center'>
-                <div className='w-full flex justify-center items-center rounded-full h-full overflow-hidden'>
-                    <img className='w-[450px] h-[500px] bg-white rounded-full' src={own} alt='image'/>
+                <div className='w-full flex  justify-center items-center rounded-full h-full overflow-hidden'>
+                    <img className='w-[450px] h-[500px] bg-[#ffe4c4] rounded-full' src={own} alt='image'/>
                 </div>
             </div>
         </div>
