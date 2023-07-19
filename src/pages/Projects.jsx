b36d3d751a7170c284f0b09919de2478c3de4a46
DC-21 <cholahkuboko@gmail.com>
2023-07-19 15:35:59 +0200

    first commit

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <div className='w-full md:h-screen h-auto'>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+      <div></div>
+      <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
