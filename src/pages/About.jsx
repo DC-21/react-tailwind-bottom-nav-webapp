a03baba98c8e90bd92265cf32c472979bea0c9bf
DC-21 <cholahkuboko@gmail.com>
2023-07-19 08:49:18 +0200

    first commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import me from '../images/me.png';
+import own from '../images/own.png';
 
 const About = () => {
   return (
     <div className='w-full flex justify-center items-center md:h-screen h-auto bg-[#f6a98c]'>
-        <div className='w-full md:flex flex-col'>
-            <div className='w-1/2 items-center justify-center'>
-                <div></div>
+        <div className='w-full md:flex justify-center items-center'>
+            <div className='w-1/2 flex items-center justify-center'>
+                <div>
+                    <p>Hello</p>
+                </div>
             </div>
-            <div className='w-1/2 items-center justify-center'>
-                <div className='w-full justify-center rounded-full'>
-                    <img className='' src={me} alt='image'/>
+            <div className='w-1/2 flex items-center justify-center'>
+                <div className='w-full justify-center rounded-full h-[400] overflow-hidden'>
+                    <img className='object-cover w-full h-[500px] overflow-hidden bg-white' src={own} alt='image'/>
                 </div>
             </div>
         </div>
