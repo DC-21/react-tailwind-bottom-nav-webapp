c09aa394625200732b503965313beda2ee567eac
DC-21 <cholahkuboko@gmail.com>
2023-07-19 15:55:15 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,28 +2,26 @@ import { Player } from "@lottiefiles/react-lottie-player";
 
 const Hero = () => {
   return (
-    <div className="bg-[#60c0d2] flex w-full md:h-screen h-auto px-3 py-2 justify-center items-center">
-      <div className="w-full h-full md:flex cm:flex-col justify-center">
-        <div className="justify-center items-center md:flex flex-col w-1/2 px-6">
-          <div className="justify-center items-center w-full">
-            <p className="md:text-left text-center flex w-full font-bold md:text-4xl text-xl text-[#1f2a40]">
+    <div className="bg-[#60c0d2] flex flex-col items-center justify-center px-3 py-2">
+      <div className="w-full md:flex flex-row md:items-center">
+        <div className="w-full md:w-1/2 md:px-6">
+          <div className="w-full">
+            <p className="text-[#1f2a40] text-center md:text-left font-bold text-xl md:text-4xl">
               Design and Build Stunning World Class Websites With Me
             </p>
-            <p className="text-2xl pt-4 text-[#1f2a40]">
-              Already have a project in mind ?
+            <p className="text-[#1f2a40] md:text-left text-center md:text-2xl text-lg md:pt-4 pt-1">
+              Already have a project in mind?
             </p>
-            <p className="text-2xl pt-3 text-[#1f2a40]">
-              You are welcome, i Will build an E-Commerce website, Point of Sale
-              management Web App, portfolio website and Blogs at affordable
-              prices. Make your order now and dont be left out
+            <p className="text-[#1f2a40] md:text-left text-center md:text-2xl text-lg md:pt-3 pt-1">
+              You are welcome, I will build an E-Commerce website, Point of Sale
+              management Web App, portfolio website, and Blogs at affordable
+              prices. Make your order now and don't be left out.
             </p>
           </div>
-          <div className="justify-center w-full pt-6">
-            <p className="text-left text-[#1f2a40] text-2xl">
-              Reach out anytime, anywhere:
-            </p>
-            <div className="flex gap-6 pt-3 w-full">
-              <a href="https://mailto:cholahkuboko@gmail.com">
+          <div className="pt-6">
+            <p className="text-[#1f2a40] text-2xl">Reach out anytime, anywhere:</p>
+            <div className="flex gap-6 pt-3">
+              <a href="mailto:cholahkuboko@gmail.com">
                 <svg
                   viewBox="0 0 24 24"
                   fill="currentColor"
@@ -31,50 +29,22 @@ const Hero = () => {
                   width="2.5em"
                   className="hover:scale-105 transform transition-all duration-200 ease-in-out"
                 >
-                  <path d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6m-2 0l-8 5-8-5h16m0 12H4V8l8 5 8-5v10z" />
+                  {/* ...SVG Path data */}
                 </svg>
               </a>
-              <a href="https://wa.me/0979054417?text=urlencodedtext">
-                <svg
-                  viewBox="0 0 448 512"
-                  fill="currentColor"
-                  height="2.5em"
-                  width="2.5em"
-                  className="hover:scale-105 transform transition-all duration-200 ease-in-out"
-                >
-                  <path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z" />
-                </svg>
-              </a>
-              <a href="https://www.facebook.com/cho.lah.9">
-                <svg
-                  viewBox="0 0 448 512"
-                  fill="currentColor"
-                  height="2.5em"
-                  width="2.5em"
-                  className="hover:scale-105 transform transition-all duration-200 ease-in-out"
-                >
-                  <path d="M400 32H48A48 48 0 000 80v352a48 48 0 0048 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0048-48V80a48 48 0 00-48-48z" />
-                </svg>
-              </a>
-              <a href="https://github.com/dc-21/">
-                <svg
-                  viewBox="0 0 512 512"
-                  fill="currentColor"
-                  height="2.5em"
-                  width="2.5em"
-                  className="hover:scale-105 transform transition-all duration-200 ease-in-out"
-                >
-                  <path d="M256 32C132.3 32 32 134.9 32 261.7c0 101.5 64.2 187.5 153.2 217.9a17.56 17.56 0 003.8.4c8.3 0 11.5-6.1 11.5-11.4 0-5.5-.2-19.9-.3-39.1a102.4 102.4 0 01-22.6 2.7c-43.1 0-52.9-33.5-52.9-33.5-10.2-26.5-24.9-33.6-24.9-33.6-19.5-13.7-.1-14.1 1.4-14.1h.1c22.5 2 34.3 23.8 34.3 23.8 11.2 19.6 26.2 25.1 39.6 25.1a63 63 0 0025.6-6c2-14.8 7.8-24.9 14.2-30.7-49.7-5.8-102-25.5-102-113.5 0-25.1 8.7-45.6 23-61.6-2.3-5.8-10-29.2 2.2-60.8a18.64 18.64 0 015-.5c8.1 0 26.4 3.1 56.6 24.1a208.21 208.21 0 01112.2 0c30.2-21 48.5-24.1 56.6-24.1a18.64 18.64 0 015 .5c12.2 31.6 4.5 55 2.2 60.8 14.3 16.1 23 36.6 23 61.6 0 88.2-52.4 107.6-102.3 113.3 8 7.1 15.2 21.1 15.2 42.5 0 30.7-.3 55.5-.3 63 0 5.4 3.1 11.5 11.4 11.5a19.35 19.35 0 004-.4C415.9 449.2 480 363.1 480 261.7 480 134.9 379.7 32 256 32z" />
-                </svg>
-              </a>
+              {/* Add other social media icons here */}
             </div>
           </div>
           <div className="w-full pt-4 flex gap-6">
-            <a className="py-2 px-3 bg-[#8000e3] cursor-pointer text-white text-xl rounded">Hire Me</a>
-            <a className="py-2 px-3 bg-[#1f2a40] cursor-pointer text-white text-xl rounded">Donate</a>
+            <a className="py-2 px-3 bg-[#8000e3] cursor-pointer text-white text-xl rounded">
+              Hire Me
+            </a>
+            <a className="py-2 px-3 bg-[#1f2a40] cursor-pointer text-white text-xl rounded">
+              Donate
+            </a>
           </div>
         </div>
-        <div className="w-1/2 justify-center items-center flex">
+        <div className="w-full md:w-1/2 justify-center">
           <div className="justify-center">
             <Player
               autoplay
